62e73d743a1524fbe8ea60d471b969d744e5d1f9
Mateusz Lapsa-Malawski <mateusz@munhitsu.com>
2012-03-24 20:59:55 -0400

    initial revision

---
+++ isi/__init__.py
@@ -0,0 +1,50 @@
+from flask import Flask
+
+from flask.ext.jsonpages import JSONPages
+from flask import render_template
+from subprocess import Popen, PIPE
+
+
+app = Flask(__name__)
+app.config.from_pyfile('settings.cfg')
+pages = JSONPages(app)
+for page in pages:
+    print page
+
+@app.route('/')
+def index():
+    page = pages.get_or_404('index')
+    template = page.content.get('template', 'default.html')
+    return render_template(template, page=page)
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    template = page.content.get('template', 'default.html')
+    return render_template(template, page=page)
+
+#github hook
+#TODO: move to separate module
+app.config.setdefault('GITHUBHOOK_CMD', "git pull")
+
+#TODO: add throttling
+@app.route("/githubhook", methods=['POST'])
+def githubhook():
+    def system(cmd):
+        print ''.join(Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=True).communicate())
+
+    cmd = app.config['GITHUBHOOK_CMD']
+
+    print "Received POST, running: %s" % cmd
+    system(cmd)
+    return 'ok'
+
+
+
+
+def main():
+    app.debug = True
+    app.run()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name='isi',
+      version='0.1-pre1',
+      description='',
+      author='',
+      author_email='',
+      packages=['isi'],
+      package_dir={'': ''},
+      install_requires=['gevent', 'flask', 'Flask-JSONPages'],
+      entry_points={
+          'console_scripts' : [
+              'runserver=isi:main',
+              ],
+          }
+      )
