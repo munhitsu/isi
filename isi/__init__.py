7c3045f7b2c513a2ca832c9beefa30e535b30370
Mateusz Lapsa-Malawski <mateusz@munhitsu.com>
2012-04-09 09:05:52 -0400

    upgrade to Flask-JSONPages 0.2

--- isi/__init__.py
+++ isi/__init__.py
@@ -3,26 +3,34 @@ from flask import Flask
 from flask.ext.jsonpages import JSONPages
 from flask import render_template
 from subprocess import Popen, PIPE
+from flask.helpers import send_from_directory
+import os.path
+from werkzeug.exceptions import NotFound
 
+app = Flask(__name__)
 
-app = Flask(__name__, static_url_path='')
 #TODO: disable static="" on production not to duplicate nginx check
 app.config.from_pyfile('settings.cfg')
 pages = JSONPages(app)
-for page in pages:
-    print page
 
 @app.route('/')
 def index():
-    page = pages.get_or_404('index')
+    page = pages.get_or_404('')
     template = page.content.get('template', 'default.html')
-    return render_template(template, page=page)
+    return render_template(template, **page.content)
 
 @app.route('/<path:path>/')
 def page(path):
+    #as werkzeug uses unordered dictionary for url mapping, let's hack for static files
+    try:
+        return send_from_directory(os.path.join(app.root_path,"static"), path, as_attachment=True)
+    except NotFound:
+        pass
     page = pages.get_or_404(path)
     template = page.content.get('template', 'default.html')
-    return render_template(template, page=page)
+    return render_template(template, **page.content)
+
+
 
 #github hook
 #TODO: move to separate module
@@ -40,8 +48,6 @@ def githubhook():
     print "Received POST, running: %s" % cmd
     system(cmd)
     return 'ok'
-
-
 
 
 def main():
